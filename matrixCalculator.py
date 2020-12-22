f027b027e33b64489433e2bfe63429f5ffd764b5
Michael Dorado <52438439+mdoradocode@users.noreply.github.com>
2020-12-21 22:39:13 -0800

    streamlining user input

--- matrixCalculator.py
+++ matrixCalculator.py
@@ -1,10 +1,16 @@
+#First we are gonna get the user to build a matrix easy right?
+#So second we are going to implement the three row operations Replacement, Switching, Scaling
 import math
+import numpy as np
 
 def main():
-    matrix = [[1,2,3],[3,4,5],[6,7,8]]
-    for y in range(0,3):
-        print(*matrix[y], sep='  ')
-        
+    rowSize, colSize = input("Please enter your matrix size (ex. 3,4 for a 3x4 matrix): ").split(",")
+    print("Enter your matrix values as a single line, left to right, row 0 to row x")
+    entries = list(map(int, input().split()))
+    mainMatrix = np.array(entries).reshape(rowSize, colSize)
+    print(*mainMatrix, sep='    ')
+
+
 
 
 main()
