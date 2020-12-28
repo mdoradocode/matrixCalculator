a542c81084094071a3a8effcbfc372fba0483f64
Michael Dorado <52438439+mdoradocode@users.noreply.github.com>
2020-12-28 01:50:51 -0800

    Implemeted Row operations

--- matrixCalculator.py
+++ matrixCalculator.py
@@ -1,5 +1,8 @@
-#First we are gonna get the user to build a matrix easy right?
-#So second we are going to implement the three row operations Replacement, Switching, Scaling
+##Matrix Row Reduction Calculator
+##Date: Winter 2020
+##Author: Michael Dorado
+##Description: This calculator will take in a users input and conert it to a matrix of which it can calculate the 
+##REF and RREF
 import math
 import sys
 import os
@@ -11,16 +14,22 @@ def main():
     entries = list(map(int, input("Enter your matrix values as a single line, left to right, row 0 to row x: ").split()))
     x = Matrix(rowSize,colSize,entries)
     x.printMainMatrix()
-    x.replace(2,1,2)
-    x.printMainMatrix()
+    x.replace(2,0,1)
+    x. scale(2,1)
+    x.switch(0,1)
+    x.printREFMatrix()
 
+##This is the matrix class, it will hold the different form that the calcuator will find
+##The algorithim for finding ithe forms wil be done in the main
 class Matrix:
     def __init__(self,row,col, entries):
         self.rowSize = row
         self.colSize = col
+        self.colIndex = col-1
+        self.rowIndex = row-1
         self.mainMatrix = np.array(entries).reshape(self.rowSize, self.colSize)
         self.REFMatrix = np.array(entries).reshape(self.rowSize, self.colSize)
-        self.RREFMatrix = np.array(0).reshape(self.rowSize, self.colSize)
+        self.storageMatrix = np.array(entries).reshape(self.rowSize, self.colSize)
     
     def printMainMatrix(self):
         for x in range(0, self.rowSize):
@@ -30,9 +39,32 @@ class Matrix:
                 else:
                     print(self.mainMatrix[x][y], sep='   ', end= ' ')
     
+    def printREFMatrix(self):
+        for x in range(0, self.rowSize):
+            for y in range(0,self.colSize):
+                if(y == self.colSize-1):
+                    print(self.REFMatrix[x][y], sep='   ', end='\n')
+                else:
+                    print(self.REFMatrix[x][y], sep='   ', end= ' ')
+
     def replace(self, scale, rA, rB):
-        for x in range(0,self.colSize):
-            self.matrix[rA] = self.matrix[rA]+scale*self.matrix[rB]
+        self.REFMatrix[rA] = self.REFMatrix[rA]+scale*self.REFMatrix[rB]
+
+    def scale(self, scale, row):
+        self.REFMatrix[row] = scale*self.REFMatrix[row]
+
+    def switch(self, rA, rB):
+        self.storageMatrix[0] =  self.REFMatrix[rA]
+        self.REFMatrix[rA] =  self.REFMatrix[rB]
+        self.REFMatrix[rB] = self.storageMatrix[0]
+
+def matchRow():
+    
+
+
+##def  findREF(matrix):
+
+
 
 
 
